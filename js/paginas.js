5d6eab73ffd6e244e2dd273aeff84a3f75404171
Omar Noyola <omar.noyolam@gmail.com>
2022-11-28 16:27:02 -0600

    :memo: Paginas 1-5

---
+++ js/paginas.js
@@ -0,0 +1,18 @@
+const paginas = [];
+
+function generarPaginas(){
+    const totalPaginas = 9;
+
+    for(let contador = 1; contador<=totalPaginas; contador++){
+        let nombrePagina;
+
+        if(contador < 10){
+            nombrePagina = `pagina0${contador}.html`;
+        }
+        else{
+            nombrePagina = `pagina${contador}.html`;
+        }
+
+        paginas.push(nombrePagina);
+    }
+}
